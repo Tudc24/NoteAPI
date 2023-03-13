b0592dbdcc3016da8d8d6f807b63652b4d7d0882
Tudc24 <tudctest@gmail.com>
2023-03-13 13:31:51 +0700

    new

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,7 @@
 const userModel = require("../models/user");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const SECRET_KEY = process.env.SECRET_KEY;
+const SECRET_KEY = "NOTESAPI";
 
 const singup =  async (req, res) =>{
 
